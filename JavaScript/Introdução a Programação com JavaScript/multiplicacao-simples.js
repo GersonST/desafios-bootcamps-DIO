d71f4f6ecbe32cc05340624b8f507afa9c48b16e
GersonST <gersonsevero90@gmail.com>
2021-04-22 05:47:58 -0300

    multiplicação simples mod 2

--- JavaScript/Introdução a Programação com JavaScript/multiplicacao-simples.js
+++ JavaScript/Introdução a Programação com JavaScript/multiplicacao-simples.js
@@ -7,7 +7,7 @@ mostrando está de acordo com a mensagem de saída esperada (exemplo abaixo).
 A entrada contém 2 valores inteiros.
 - Saída
 Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um
-espaço em branco antes e depois da igualdade... */
+espaço em branco antes e depois da igualdade.... */
 
 let valor1 = parseInt(gets());
 let valor2 = parseInt(gets());
